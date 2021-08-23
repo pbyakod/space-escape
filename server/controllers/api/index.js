b5852d58fdbb2c9bf68f0a1e99a77bff6eabe15c
Dmartin <dmartin4820@yahoo.comgit config --global user.email dmartin4822@yahoo.com>
2021-08-22 20:05:54 -0700

    added gameRoutes and updated index with correct routes

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -5,12 +5,17 @@ const obstacleRoutes = require('./obstacleRoutes');
 const resultRoutes = require('./resultRoutes');
 const scoreRoutes = require('./scoreRoutes');
 const storyRoutes = require('./storyRoutes');
+const userRoutes = require('./userRoutes');
+const gameRoutes = require('./gameRoutes');
+
 
 router.use('/gameState', gameStateRoutes);
 router.use('/leaderBoard', leaderBoardRoutes);
 router.use('/obstacle', obstacleRoutes);
 router.use('/result', resultRoutes);
-router.use('score', scoreRoutes);
-router.use('story', storyRoutes);
+router.use('/score', scoreRoutes);
+router.use('/story', storyRoutes);
+router.use('/game', gameRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
