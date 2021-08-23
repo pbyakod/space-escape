7383299f392058dd6c8f52c81d9807be3b5d474f
Pranav Byakod <pranav.byakod@gmail.com>
2021-08-23 15:28:36 -0700

    adding GET route for obstacleRoutes.js

--- server/controllers/api/obstacleRoutes.js
+++ server/controllers/api/obstacleRoutes.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const { Encounter } = require('../../models');
 
 router.get('/', (req,res) => {
-	res.json({message: 'Testing'});
+	try {
+		const encounter = await Encounter.findAndCountAll();
+		res.json({ encounter });
+	} catch(err) {
+		res.json({ err });
+	}
 })
 
 module.exports = router;
