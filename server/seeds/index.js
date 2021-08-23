0ff54e0f10c8b89b66a43b074e21dabce4b2d9e7
Dmartin <dmartin4820@yahoo.comgit config --global user.email dmartin4822@yahoo.com>
2021-08-23 16:29:28 -0700

    modify seeds to fit new model structure

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -1,19 +1,16 @@
 const charProto = require('./charProtoSeeds');
 const encounter = require('./encounterSeeds');
 const game = require('./gameSeeds');
-const gameState = require('./gameStateSeeds');
 const location = require('./locationSeeds');
 const user = require('./userSeeds');
 const sequelize = require('../config/connection');
 
 async function seedDB() {
 	await sequelize.sync({force: true});
-
 	await charProto.seed();
 	await encounter.seed();
 	await location.seed();
 	await user.seed();
-	await gameState.seed();
 	await game.seed();
 }
 
