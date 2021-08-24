dfc5f18e30ea7372c0cbb212dcdafe6550da1f9c
qtian13 <qiushuang.tian@gmail.com>
2021-08-23 18:31:30 -0700

    format

--- server/controllers/api/userRoutes.js
+++ server/controllers/api/userRoutes.js
@@ -1,14 +1,14 @@
-const { User, Game, CharProto, Location } = require('../../models');
-const router = require('express').Router();
+const { User, Game, CharProto, Location } = require("../../models");
+const router = require("express").Router();
 
 // GET user info
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const dbUserData = await User.findByPk(req.session.userId, {
-      attributes: { exclude: ['password'] },
+      attributes: { exclude: ["password"] },
     });
-		res.status(200).json(dbUserData);
+    res.status(200).json(dbUserData);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -16,11 +16,11 @@ router.get('/', async (req, res) => {
 });
 
 // GET user info with related games info
-router.get('/details', async (req, res) => {
+router.get("/details", async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const dbUserData = await User.findByPk(req.session.userId, {
-      attributes: { exclude: ['password'] },
+      attributes: { exclude: ["password"] },
       include: [
         {
           model: Game,
@@ -28,7 +28,7 @@ router.get('/details', async (req, res) => {
             {
               model: CharProto,
             },
-						{
+            {
               model: Location,
             },
           ],
@@ -36,7 +36,7 @@ router.get('/details', async (req, res) => {
       ],
       // order: [[Game, 'date_created', 'DESC']],
     });
-		res.status(200).json(dbUserData);
+    res.status(200).json(dbUserData);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -44,7 +44,7 @@ router.get('/details', async (req, res) => {
 });
 
 // CREATE a new user
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const dbUserData = await User.create(req.body);
 
@@ -61,7 +61,7 @@ router.post('/', async (req, res) => {
 });
 
 // login to an existing account
-router.post('/login', async (req,res) => {
+router.post("/login", async (req, res) => {
   if (!req.body) {
     res.status(400).end();
   }
@@ -70,34 +70,35 @@ router.post('/login', async (req,res) => {
     const dbUserData = await User.findOne({
       where: {
         username: req.body.username,
-      }
+      },
     });
 
     if (!user) {
-      res.status(404).json({message: 'Cannot find user'});
+      res.status(404).json({ message: "Cannot find user" });
     }
 
     // const isValidPass = await bcrypt.compare(req.body.password, user.password);
-		const validPassword = await dbUserData.checkPassword(req.body.password);
+    const validPassword = await dbUserData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res.status(400).json({ message: 'Invalid username or password' });
-		}
-		req.session.save(() => {
-			req.session.loggedIn = true;
-			req.session.username = dbUserData.get({ plain: true }).username;
-			req.session.userId = dbUserData.get({ plain: true }).id;
-			console.log(req.session)
-			res.status(200).json({ user: dbUserData, message: 'You are now logged in!' });
-		})
-  } catch(err) {
-    console.log(err),
-		res.status(500).json(err);
+      res.status(400).json({ message: "Invalid username or password" });
+    }
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      req.session.username = dbUserData.get({ plain: true }).username;
+      req.session.userId = dbUserData.get({ plain: true }).id;
+      console.log(req.session);
+      res
+        .status(200)
+        .json({ user: dbUserData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    console.log(err), res.status(500).json(err);
   }
-})
+});
 
 // logout the current account and destroy the session
-router.post('/logout', (req, res) => {
+router.post("/logout", (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
