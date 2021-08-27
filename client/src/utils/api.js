3f26f66907206fa96fed8eb036f63ed0f1ef3e23
Dmartin <dmartin4820@yahoo.comgit config --global user.email dmartin4822@yahoo.com>
2021-08-26 20:21:15 -0700

    add functions to GET user games, character prototypes, and get encounter and POST route for game

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -17,7 +17,7 @@ function UserBodyError(body) {
 async function login(body) {
   UserBodyError(body);
   const response = await axios.post("/api/user/login", body);
-  return response;
+  return response.data;
 }
 
 // Fetch request for signing up a user. Returns a promise
@@ -30,7 +30,7 @@ async function login(body) {
 async function signUp(body) {
   UserBodyError(body);
   const response = await axios.post("/api/user", body);
-  return response;
+  return response.data;
 }
 
 // Fetch request for signing up a user. Returns a promise
@@ -44,11 +44,41 @@ async function getUserGames(userId) {
   if (typeof userId !== "number") {
     throw new Error("Input must be a Number type");
   }
-  return await axios.get(`/api/game/${userId}`);
+	const response = await axios.get(`/api/game/${userId}`);
+  return response.data;
 }
 
-export default {
+async function getCharPrototypes() {
+	const response = await axios.get("/api/charProto");
+  return response.data; 
+}
+
+async function createGame(user_id, body) {
+	if (!user_id || !body.char_id || !body.health || !body.ship || !body.gold || !body.location_id) {
+		console.log(user_id, body)
+		throw new Error("Arguments require user_id, char_id, location_id, health, ship, and gold");
+	}
+	const response = await axios.post('/api/game', {user_id, ...body});
+	return response.data;
+}
+
+
+async function getEncounter(location_id) {
+	if (!location_id) {
+		throw new Error("Must pass location id");
+	}
+	const response = await axios.get(`/api/encounter/${location_id}`);
+	return response.data;
+}
+
+const apiCalls = {
   getUserGames,
   login,
   signUp,
-};
+	getCharPrototypes,
+	createGame,
+	getEncounter
+
+}; 
+
+export default apiCalls; 
