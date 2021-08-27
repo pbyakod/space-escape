6cd01d6af1cb3d6c66c6299cb7c94a9c5c89741c
Pranav Byakod <pranav.byakod@gmail.com>
2021-08-27 00:21:26 -0700

    generated style for auth.jsx

--- client/src/components/pages/Auth.jsx
+++ client/src/components/pages/Auth.jsx
@@ -1,6 +1,7 @@
 // import { useState, useEffect } from "react"
 // import ContainerSmall from "./ContainerSmall"
 import { Link } from "react-router-dom"
+import "../../styling/auth.scss"
 export default function auth({setAuth}) {
   //e.target to handle form
       //client side check username and password
@@ -20,32 +21,75 @@ export default function auth({setAuth}) {
     }
   return (
     <div>
-    
-      <section className="containerLarge">
-        <h1 className="title">Auth Page</h1>
+      {/* <section className="containerLarge">
         <article className="containerSmall">
-          <div className="">
-        <h2 className="title">Sign In</h2>
-        <p>username</p>
-        <input type="text" id='username'/>
-        <p>password</p>
-        <input type="text" id='password'/>
-        <button onClick={auth}>submit</button>
-        </div>
-        <div className="">
-        <h2 className="title">Sign Up</h2>
-        <p>username</p>
-        <input type="text"/>
-        <p>password</p>
-        <input type="text" />
-        <button>submit</button>
-        </div>
+          <div className="flex-child">
+            <h2 className="title">Sign In</h2>
+            <p>username</p>
+            <input type="text" id='username'/>
+            <p>password</p>
+            <input type="text" id='password'/>
+            <button onClick={auth}>submit</button>
+          </div>
+          <div className="flex-child">
+            <h2 className="title">Sign Up</h2>
+            <p>username</p>
+            <input type="text"/>
+            <p>password</p>
+            <input type="text" />
+            <button>submit</button>
+            </div>
         </article>
-
-      <Link to="" exact>
-      <button>back</button>
-      </Link>
-      </section>
+        <Link to="" exact>
+          <button>back</button>
+        </Link>
+      </section> */}
+      <div className="container">
+        <div className="container-wrapper">
+          <div className="card-background">
+            <div className="card-items">
+              <h2 className="title">Sign In</h2>
+            </div>
+            <div className="card-items">
+              <p>username</p>
+              <input type="text" id='username'/>
+            </div>
+            <div className="card-items">
+                <p>password</p>
+                <input type="text" id='password'/>
+            </div>
+            <div className="card-items">
+              <button onClick={auth}>submit</button>
+            </div>
+            <div className="card-items">
+              <Link to="" exact>
+                <button>back</button>
+              </Link>
+            </div>
+          </div>
+          <div className="card-background">
+            <div className="card-items">
+              <h2 className="title">Sign Up</h2>
+            </div>
+            <div className="card-items">
+              <p>username</p>
+              <input type="text" id='username'/>
+            </div>
+            <div className="card-items">
+                <p>password</p>
+              <input type="text" id='password'/>
+            </div>
+            <div className="card-items">
+              <button onClick={auth}>submit</button>
+            </div>
+            <div className="card-items">
+              <Link to="" exact>
+                <button>back</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
