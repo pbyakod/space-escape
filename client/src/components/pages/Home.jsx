7b1ee6a24ab9dfdab48e5fff8fff2aabc3d2b247
Pranav Byakod <pranav.byakod@gmail.com>
2021-08-26 23:51:24 -0700

    generated stylee for home.jsx

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,16 +1,27 @@
 import { Link } from "react-router-dom";
+import "../../styling/home.scss"
 
 export default function home() {
   return (
     <div className="homepage">
-      <section className="containerLarge">
-        <h1>Home Page</h1>
-        <Link to="auth">Sign in / Sign up</Link>
-        <Link to="rules">How to Play</Link>
-        <Link to="about">About the Authors</Link>
+      <section className="containerLarge1">
+        <section className="containerTitle">
+          <h1>Space Escape</h1>
+        </section>
+        <section className="containerList">
+          <div className="containerLink">
+            <Link to="auth">Sign in / Sign up</Link>
+          </div>
+          <div className="containerLink">
+            <Link to="rules">How to Play</Link>
+          </div>
+          <div className="containerLink">
+            <Link to="about">About the Authors</Link>
+          </div>
+        </section>
       </section>
 
-      <section className="containerLarge">
+      <section className="containerLarge2">
         <h1>Home Page</h1>
         <Link to="story">Continue Game</Link>
         <Link to="createCharacter">New Game</Link>
