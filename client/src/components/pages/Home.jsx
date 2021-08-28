7d780a5b470b82d11d51863f7d30e1f5004536c1
Pranav Byakod <pranav.byakod@gmail.com>
2021-08-27 19:22:35 -0700

    creating template main container

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 export default function home() {
   return (
     <div className="homepage">
-      <section className="containerLarge">
+      <section className="main-container">
         <h1>Home Page</h1>
-        <Link to="auth">Sign in / Sign up</Link>
-        <Link to="rules">How to Play</Link>
-        <Link to="about">About the Authors</Link>
+        <section className="container-content">
+          <Link to="auth">Sign in / Sign up</Link>
+          <Link to="rules">How to Play</Link>
+          <Link to="about">About the Authors</Link>
+        </section>
       </section>
 
       <section className="containerLarge">
