d9714a6e2688999530f1ecaea8b92a8b0d3d7b1e
Dmartin <dom4822@yahoo.com>
2021-08-27 23:12:56 -0700

    add user token to localStorage in the relevant functions

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,37 +1,100 @@
 import axios from "axios";
-// const axios = require('axios');
+import decode from "jwt-decode";
+
+function getTokenHeader() {
+	const token = getToken(); 
+	return token ? `Bearer ${token}` : '';
+}
+
+function getToken() {
+	return JSON.parse(localStorage.getItem('user')).token;
+}
 
 function UserBodyError(body) {
   if (!body.username || !body.password) {
     throw new Error("Need both username and password");
   }
 }
+
+// Check the stored user data in localStorage. If the current time is beyond 
+// the token expiration, delete the user and return false, meaning the user is
+// not logged in anymore. Otherwise they are logged in
+// inputs:
+// 		N/A
+// ouputs:
+// 		(Boolean)
+function loggedIn() {
+	try {
+		const token = getToken();
+		const decodedToken = decode(token);
+
+		if (decodedToken.exp < Date.now()/1000) {
+			localStorage.removeItem('user');
+			return false;
+		} else {
+			return true;
+		}
+	} catch(err) {
+		throw err
+	}
+}
 // Fetch request for logging in a user. Returns a promise containing
 // object with a user object ({id, name, health, ship, gold} = user)
-// and a message with value string
+// and a message with value string. The response should also contain a 
+// token which is stored in localstorage along with user info.
 //
 // inputs:
 // 		body (object): {username, password}
 // outputs:
-// 		response (promise): promise({user, message})
+// 		response.data (promise): promise({user, message, token})
 async function login(body) {
   UserBodyError(body);
-  const response = await axios.post("/api/user/login", body);
-  return response.data;
+	try {
+ 		const response = await axios.post("/api/user/login", body);
+
+		if (response.statusText === 'OK') {
+			console.log('response good', response.data);
+			localStorage.setItem('user', JSON.stringify(response.data));
+		} else {
+			return {};
+		}
+
+		return response.data 
+	} catch(err) {
+		throw err;
+	}
 }
 
 // Fetch request for signing up a user. Returns a promise
-// containing an object for the newly created user ({id, name, health, ship, gold} = user)
+// containing an object for the newly created user ({id, name, health, ship, gold} = user).
+// The response should also contain a token which is stored in localstorage along with user info.
 //
 // inputs:
 // 		body (object): {username, password}
 // outputs:
-// 		response (promise): promise({dbUserData})
+// 		response.data (promise): promise({id, username, createdAt, token})
 async function signUp(body) {
   UserBodyError(body);
-  const response = await axios.post("/api/user", body);
-  return response.data;
+	try {
+  	const response = await axios.post("/api/user", body);
+
+		if (response.statusText === 'OK') {
+			console.log('response ok', response.data);
+			localStorage.setItem('user', JSON.stringify(response.data));
+		} else {
+			return {};
+		}
+
+		return response.data;
+	} catch(err) {
+		throw err;
+	}
 }
+
+// function logout() {
+// 	localStorage.removeItem('user');
+// 	window.location.assign('/');
+// }
 
 // Fetch request for signing up a user. Returns a promise
 // containing an object for the newly created user ({id, name, health, ship, gold} = user)
@@ -48,11 +111,25 @@ async function getUserGames(userId) {
   return response.data;
 }
 
+// GET request for all available characters for a user to pick from
+// 
+// inputs:
+// 		N/A
+// outputs:
+// 		response.data (promise): promise([{id, name, health, ship, gold}])
 async function getCharPrototypes() {
 	const response = await axios.get("/api/charProto");
   return response.data; 
 }
 
+
+// POST request to create a new game given the user_id, character the user chose, and the first location_id
+//
+// inputs:
+// 		user_id (Number): Specifier for user
+// 		body (object): {char_id, health, ship, gold, location_id}
+// outputs:
+// 		response.data (promise): promise({id, inProgress, user_id, char_id, health, ship, gold})
 async function createGame(user_id, body) {
 	if (!user_id || !body.char_id || !body.health || !body.ship || !body.gold || !body.location_id) {
 		console.log(user_id, body)
@@ -62,7 +139,12 @@ async function createGame(user_id, body) {
 	return response.data;
 }
 
-
+// GET request to get all encounters associated with a single location
+//
+// inputs:
+// 		location_id (Number)
+// outputs:
+// 		response.data (promise): Array of encounters as a promise; promise([{id, message, option1, option2, location_id, Location{id, name, createdAt, updatedAt}}, {...}...]])
 async function getEncounter(location_id) {
 	if (!location_id) {
 		throw new Error("Must pass location id");
@@ -77,7 +159,8 @@ const apiCalls = {
   signUp,
 	getCharPrototypes,
 	createGame,
-	getEncounter
+	getEncounter,
+	loggedIn
 
 }; 
 
