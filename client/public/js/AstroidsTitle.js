4263725fc2e6fc3562eabd89f361134ede0b1c96
qtian13 <qiushuang.tian@gmail.com>
2021-08-29 17:04:23 -0700

    connect to anime.js

--- client/public/js/AstroidsTitle.js
+++ client/public/js/AstroidsTitle.js
@@ -1,6 +1,29 @@
 const container = document.querySelector('.container');
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 100; i++) {
   const stock = document.createElement('div');
   stock.classList.add('stock');
   container.appendChild(stock);
 }
+
+// function animateStocks() {
+//   anime({
+//     target: '.stock',
+//     translateX: 40,
+//   })
+// }
+
+// animateStocks()
+
+anime({
+  targets: '.stock',
+  translateX: function() {
+    return anime.random(-700, 700);
+  },
+  translateY: function() {
+    return anime.random(-700, 700);
+  },
+  scale: function() {
+    return anime.random(1, 2);
+  }
+  
+})
