872aad19caf8c1505d4091aed94b1b79c3fa34d2
Denzal Martin <43591843+dmartin4820@users.noreply.github.com>
2021-08-29 19:17:39 -0700

    Rename to asteroidRoutes.js and fix typo

--- server/controllers/astroids/asteroidRoutes.js
+++ server/controllers/astroids/asteroidRoutes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 router.get('/home', async (req, res) => {
   try {
-    res.sendFile('/AstroidsTitle.html');
+    res.sendFile('/AsteroidsTitle.html');
   } catch (err) {
     res.status(500).json(err);
   }
@@ -11,7 +11,7 @@ router.get('/home', async (req, res) => {
 
 router.post('/start', async (req, res) => {
   try {
-    res.sendFile('/AstroidsStart.html');
+    res.sendFile('/AsteroidsStart.html');
   } catch (err) {
     res.status(500).json(err);
   }
