b1232e9ee2fa959d98c30c733173ca15d354def4
Dmartin <dom4822@yahoo.com>
2021-09-01 13:28:29 -0700

    create basic game context with a single create game reducer

---
+++ client/src/utils/Game/GlobalState.js
@@ -0,0 +1,25 @@
+import { createContext, useContext } from "react";
+import { useGameReducer } from "./reducers";
+
+const GameContext = createContext();
+const { Provider } = GameContext;
+
+const GameProvider = ({ value=[], ...props }) => {
+  const [state, dispatch] = useGameReducer({
+    location_id: 1,
+    encounter_id: 1,
+    char_id: null,
+    health: 0,
+    ship: 0,
+    gold: 0,
+    inProgress: false
+  });
+
+  return <Provider value={[state, dispatch]} {...props} />;
+}
+
+const useGameContext = () => {
+  return useContext(GameContext);
+}
+
+export { GameProvider, useGameContext };
