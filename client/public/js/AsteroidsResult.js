89fda63a165f1541c600179b8928b70374f0770a
qtian13 <qiushuang.tian@gmail.com>
2021-09-01 23:58:14 -0700

    add some description

--- client/public/js/AsteroidsResult.js
+++ client/public/js/AsteroidsResult.js
@@ -6,17 +6,17 @@ console.log(conclusion);
 if (score === 0) {
   title.innerText = "Thank you very much!"
   conclusion.innerHTML = `
-  <div class="my-3">Good job! You behavior brave during the fight and protect people in the planet.</div>
-  <div class="my-3">You earned xxx gold during the fight! It is enough to repair your spaceship</div>
-  <div class="my-3">As the ship is damaged badly, it will take 15 days to fix the it</div>
+  <div class="my-3">Good job! You stood out to protect people on the planet!</div>
+  <div class="my-3">You earned xxx gold during the fight! It is enough to repair your spaceship.</div>
+  <div class="my-3">The ship is damaged badly. It will take 15 days to be fully repaired.</div>
   <div class="my-3">Enjoy your tour and good luck!</div>
   `;
 } else {
   title.innerText = "Congratulations!";
   conclusion.innerHTML = `
-  <div class="my-3">Good job! You behavior brave during the fight and protect people in the planet.</div>
-  <div class="my-3">You earned xxx gold during the fight! It is enough to repair your spaceship</div>
-  <div class="my-3">It will take x days to fix the it</div>
+  <div class="my-3">Good job! You stood out to protect people on the planet!</div>
+  <div class="my-3">You earned xxx gold during the fight! It is enough to repair your spaceship.</div>
+  <div class="my-3">It will take 15 days to be fully repaired.</div>
   <div class="my-3">Enjoy your tour and good luck!</div>
   `;
 }
