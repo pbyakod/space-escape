4717268b6a6290e46b26e55230b179dddf058106
Dmartin <dom4822@yahoo.com>
2021-09-03 14:37:51 -0700

    complete logic for setting the outcome (affects character stats), handling changes to character stats, setting the next location, and rendering the end of the game

--- client/src/components/pages/game/encounter/result/Result.jsx
+++ client/src/components/pages/game/encounter/result/Result.jsx
@@ -1,15 +1,50 @@
 import { useGameContext } from "../../../../../utils/Game/GlobalState"
-import { RENDER_STORY} from "../../../../../utils/Game/actions";
+import { RENDER_STORY, RENDER_END_GAME, SET_NEXT_LOCATION, AFFECT_CHAR_STATS} from "../../../../../utils/Game/actions";
+import parseOption from "../../../../../utils/helpers";
+import {useEffect} from "react";
 
-export default function Result() {
+export default function Result({getGameData}) {
   const [state, dispatch] = useGameContext();
 
+  useEffect(() => {
+    const characterStatsChange = {
+      health: state.health + state.outcome.health,
+      gold: state.gold + state.outcome.gold,
+      ship: state.ship + state.outcome.ship
+    }
+
+    dispatch({
+      type: AFFECT_CHAR_STATS,
+      statsChange: characterStatsChange
+    })
+  }, [])
+
   function handleClick(e) {
-    dispatch({type: RENDER_STORY})
+    if (state.health <= 0 || state.gold <= 0|| state.ship <= 0) {
+      console.log('Game Over!')
+      dispatch({
+        type: RENDER_END_GAME //sets renderEndGame to true and inProgress to false
+      })
+    } else {
+      getGameData(state.outcome.next_location_id)
+        .then(GameData => {
+          console.log('here',GameData)
+          dispatch({
+            type: SET_NEXT_LOCATION, 
+            location_id: GameData.id,
+            encounter: GameData.encounters[0]
+          });
+        });
+    }
+    dispatch({type: RENDER_STORY}) //sets renderStory to true
   }
+
   return (
     <div>
       <h1>Result Page</h1>
+      {state.health}
+      {state.gold}
+      {state.ship}
       <button onClick={handleClick}>continue</button>
     </div>
   )
