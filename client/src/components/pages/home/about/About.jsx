76b917d0906c2924494b08c49925115b7a67e6b7
qtian13 <qiushuang.tian@gmail.com>
2021-09-03 10:30:33 -0700

    correct route

--- client/src/components/pages/home/about/About.jsx
+++ client/src/components/pages/home/about/About.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import authors from "../../../../aboutAuthors/about"
+import authors from "./about"
 import Author from "../author/Author"
 import './about.scss'
 import sound from "../../../../utils/sound";
