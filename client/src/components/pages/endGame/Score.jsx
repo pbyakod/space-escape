e17052019b315632156a77b9816d345d6426d115
Dmartin <dom4822@yahoo.com>
2021-09-03 22:49:40 -0700

    handle out of bounds location id (End of game) to render score page and allow the user to see the leaderboard and start a new game

--- client/src/components/pages/endGame/Score.jsx
+++ client/src/components/pages/endGame/Score.jsx
@@ -1,8 +1,26 @@
+import { Link } from "react-router-dom";
+import { INITIALIZE_GLOBAL_STATE, RENDER_LEADERBOARD } from "../../../utils/Game/actions";
+import { useGameContext } from "../../../utils/Game/GlobalState"
 
 export default function Score() {
+  const [state, dispatch] = useGameContext();
+
+  function directToLeaderboard() {
+    dispatch({
+      type: RENDER_LEADERBOARD
+    })
+  }
+
+  function startNewGame() {
+    dispatch({
+      type: INITIALIZE_GLOBAL_STATE
+    })
+    document.location.href('/createCharacter');
+  }
   return (
     <div>
       <h1>Score Page</h1>
+      <button onClick={directToLeaderboard}>Leaderboard</button>
     </div>
   )
 }
