433abb79e8974cccfebb9cbda1d7d75a419ed10d
Troy Johnson <tnj8510@gmail.com>
2021-09-06 16:34:28 -0700

    resize volume button

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FaVolumeUp, FaVolumeMute } from "react-icons/fa";
+//import { FaVolumeUp, FaVolumeMute } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import sound from "../../utils/sound";
 import "./navBar.scss";
@@ -35,7 +35,7 @@ export default function NavBar() {
       <div className="btn">
         <span className="vol-btn nav-btn" onClick={() => { 
           setSoundOn(sound.SwitchSound())
-          }}>{soundOn ? <FaVolumeUp /> : <FaVolumeMute /> }</span>
+          }}>{soundOn ? <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="2em" width="2em" xmlns="http://www.w3.org/2000/svg"><path d="M11.536 14.01A8.473 8.473 0 0014.026 8a8.473 8.473 0 00-2.49-6.01l-.708.707A7.476 7.476 0 0113.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"></path><path d="M10.121 12.596A6.48 6.48 0 0012.025 8a6.48 6.48 0 00-1.904-4.596l-.707.707A5.483 5.483 0 0111.025 8a5.483 5.483 0 01-1.61 3.89l.706.706z"></path><path d="M8.707 11.182A4.486 4.486 0 0010.025 8a4.486 4.486 0 00-1.318-3.182L8 5.525A3.489 3.489 0 019.025 8 3.49 3.49 0 018 10.475l.707.707z"></path><path fill-rule="evenodd" d="M6.717 3.55A.5.5 0 017 4v8a.5.5 0 01-.812.39L3.825 10.5H1.5A.5.5 0 011 10V6a.5.5 0 01.5-.5h2.325l2.363-1.89a.5.5 0 01.529-.06z" clip-rule="evenodd"></path></svg> : <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="2em" width="2em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6.717 3.55A.5.5 0 017 4v8a.5.5 0 01-.812.39L3.825 10.5H1.5A.5.5 0 011 10V6a.5.5 0 01.5-.5h2.325l2.363-1.89a.5.5 0 01.529-.06zm7.137 1.596a.5.5 0 010 .708l-4 4a.5.5 0 01-.708-.708l4-4a.5.5 0 01.708 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M9.146 5.146a.5.5 0 000 .708l4 4a.5.5 0 00.708-.708l-4-4a.5.5 0 00-.708 0z" clip-rule="evenodd"></path></svg> }</span>
       </div>
       <NavLink to="/map" className="nav-btn">
         <i class="fas fa-map-marked-alt fa-2x nav-btn"></i>
