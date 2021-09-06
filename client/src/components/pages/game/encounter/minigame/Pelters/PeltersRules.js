b72e45fe803af2b9307fe23ce20a46930faf8e5c
pconsag <pconsagra@g.ucla.edu>
2021-09-05 22:53:27 -0700

    add new rules

--- client/src/components/pages/game/encounter/minigame/Pelters/PeltersRules.js
+++ client/src/components/pages/game/encounter/minigame/Pelters/PeltersRules.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaCaretLeft, FaCaretRight, FaCaretUp, FaSpaceShuttle } from 'react-icons/fa';
+import {  FaCaretUp, FaCaretDown, FaSpaceShuttle } from 'react-icons/fa';
 
 export default function AsteroidsRules({ setGameProcess }) {
 
@@ -18,33 +18,19 @@ export default function AsteroidsRules({ setGameProcess }) {
       <h1 className="mt-5 text-center"> How to Play </h1>
       <div className="container">
 
-        <div className="w-50 mx-auto mt-5"><FaCaretUp />: accelarate</div>
-        <div className="w-50 mx-auto"><FaCaretLeft />: Rotate the ship counter clockwise</div>
-        <div className="w-50 mx-auto"><FaCaretRight />: Rotate the ship clockwise</div>
-        <div className="w-50 mx-auto">Space Bar: Shoot laser</div>
+        <div className="w-50 mx-auto"><FaCaretUp />: Move ship up</div>
+        <div className="w-50 mx-auto"><FaCaretDown />: Move ship  down</div>
+     
         
         <div className="mt-5 mb-3 w-50 mx-auto">
-          Asteroids move in random direction and velocity
-        </div>
-
-        <div className="mt-3 w-50 mx-auto">
-          Earn scores by hiting asteroids:
-          <div>20 pts for a large asteroids</div>
-          <div>50 pts for a medium asteroid</div>
-          <div>100 pts for a small asteroid</div>
-        </div>
-        <div className="mt-3 w-50 mx-auto">
-          A large asteroid breaks into two medium asteroids when hit
-        </div>
-        <div className="w-50 mx-auto">
-          A medium asteroid breaks into two small asteroids when hit
+          Planets come from right to left
         </div>
         
         <div className="my-3 w-50 mx-auto">
-          <FaSpaceShuttle />  Ship health decrease by 10 when hit by an asteroid
+          <FaSpaceShuttle />  Ship health decrease by 10 when hit by a planet
         </div>
         <div className="my-3 w-50 mx-auto">
-          Game ends when ship health hit 0 or all asteroids are cleared.
+          Game ends when ship health hit 0 or when you have made it 20 lightyears to home.
         </div>
         <div className="mt-5 d-flex justify-content-around ">
           <button className="btn btn-info w-25" onClick={ renderPrepare }>Continue</button>
