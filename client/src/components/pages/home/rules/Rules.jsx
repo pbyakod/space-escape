3c52b16dc570c275ef5582ec2a20170562ea679a
pconsag <pconsagra@g.ucla.edu>
2021-09-06 16:48:45 -0700

    update rules

--- client/src/components/pages/home/rules/Rules.jsx
+++ client/src/components/pages/home/rules/Rules.jsx
@@ -10,23 +10,26 @@ export default function Rules() {
       <div className="main-container">
         {/* one rule per rule-title div so that content will fit the entire container */}
         <div className="rule-title">
-          <h1>Rules Page</h1>
+          <h1>How To Play</h1>
         </div>
         {/* the p tags will be changed once we define the rules of the game */}
         <div className="rule-items">
-          <p>here are some rules</p>
+          <p>Step 1: Sign Up or Sign in!</p>
         </div>
         <div className="rule-items">
-          <p>here are some more rules</p>
+          <p>Step2: Pick your Character: Each has three values. Ship, Health, and Gold. All of these are needed to win the game, so choose wisely!</p>
         </div>
         <div className="rule-items">
-          <p>even more rules</p>
+          <p>Step 3: Play!</p>
         </div>
         <div className="rule-items">
-          <p>too many rules now</p>
+          <p>When given a choice of options, pick whicher you think is best, each has a unqique set of outcomes!</p>
         </div>
         <div className="rule-items">
-          <p>now you know how to play</p>
+          <p>When presented with an option to play a mini-game, you can either play the game, or skip it. Be warned that skipping a minigame will always depelete your health, ship, gold, or all three!</p>
+        </div>
+        <div className="rule-items">
+          <p>Make it home before your ship breaks down, or your health runs out! Enjoy!</p>
         </div>
         <div className="rule-items">
         {/* uses same properties as back-btn from home.scss */}
